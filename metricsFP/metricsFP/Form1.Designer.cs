ea14dd64940a68659943b5492942f6c78e9a25c0
Faris Osama <111440224+linzi-v7@users.noreply.github.com>
2025-05-11 19:32:47 +0300

    feat: implement UFP calculation function
    -adds simple UI + necessary functions to calculate UFP

--- metricsFP/metricsFP/Form1.Designer.cs
+++ metricsFP/metricsFP/Form1.Designer.cs
@@ -28,13 +28,266 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.extInput_Label = new System.Windows.Forms.Label();
+            this.extOutput_Label = new System.Windows.Forms.Label();
+            this.extInquiry_Label = new System.Windows.Forms.Label();
+            this.intLogicFiles_Label = new System.Windows.Forms.Label();
+            this.extInterfaceFiles_Label = new System.Windows.Forms.Label();
+            this.count_Label = new System.Windows.Forms.Label();
+            this.extInput_Numeric = new System.Windows.Forms.NumericUpDown();
+            this.extOutput_Numeric = new System.Windows.Forms.NumericUpDown();
+            this.extInquiry_Numeric = new System.Windows.Forms.NumericUpDown();
+            this.intLogicFiles_Numeric = new System.Windows.Forms.NumericUpDown();
+            this.extInterfaceFiles_Numeric = new System.Windows.Forms.NumericUpDown();
+            this.extInput_ComboBox = new System.Windows.Forms.ComboBox();
+            this.type_Label = new System.Windows.Forms.Label();
+            this.extOutput_ComboBox = new System.Windows.Forms.ComboBox();
+            this.extInquiry_ComboBox = new System.Windows.Forms.ComboBox();
+            this.intLogicFiles_ComboBox = new System.Windows.Forms.ComboBox();
+            this.extInterfaceFiles_ComboBox = new System.Windows.Forms.ComboBox();
+            this.resultTextBox = new System.Windows.Forms.RichTextBox();
+            this.calculateUFP_Button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.extInput_Numeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.extOutput_Numeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.extInquiry_Numeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.intLogicFiles_Numeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.extInterfaceFiles_Numeric)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // extInput_Label
+            // 
+            this.extInput_Label.AutoSize = true;
+            this.extInput_Label.Location = new System.Drawing.Point(67, 70);
+            this.extInput_Label.Name = "extInput_Label";
+            this.extInput_Label.Size = new System.Drawing.Size(86, 16);
+            this.extInput_Label.TabIndex = 0;
+            this.extInput_Label.Text = "External Input";
+            // 
+            // extOutput_Label
+            // 
+            this.extOutput_Label.AutoSize = true;
+            this.extOutput_Label.Location = new System.Drawing.Point(68, 116);
+            this.extOutput_Label.Name = "extOutput_Label";
+            this.extOutput_Label.Size = new System.Drawing.Size(96, 16);
+            this.extOutput_Label.TabIndex = 1;
+            this.extOutput_Label.Text = "External Output";
+            // 
+            // extInquiry_Label
+            // 
+            this.extInquiry_Label.AutoSize = true;
+            this.extInquiry_Label.Location = new System.Drawing.Point(67, 166);
+            this.extInquiry_Label.Name = "extInquiry_Label";
+            this.extInquiry_Label.Size = new System.Drawing.Size(97, 16);
+            this.extInquiry_Label.TabIndex = 2;
+            this.extInquiry_Label.Text = "External Inquiry";
+            // 
+            // intLogicFiles_Label
+            // 
+            this.intLogicFiles_Label.AutoSize = true;
+            this.intLogicFiles_Label.Location = new System.Drawing.Point(67, 206);
+            this.intLogicFiles_Label.Name = "intLogicFiles_Label";
+            this.intLogicFiles_Label.Size = new System.Drawing.Size(129, 16);
+            this.intLogicFiles_Label.TabIndex = 3;
+            this.intLogicFiles_Label.Text = "Internal Logical Files";
+            // 
+            // extInterfaceFiles_Label
+            // 
+            this.extInterfaceFiles_Label.AutoSize = true;
+            this.extInterfaceFiles_Label.Location = new System.Drawing.Point(67, 255);
+            this.extInterfaceFiles_Label.Name = "extInterfaceFiles_Label";
+            this.extInterfaceFiles_Label.Size = new System.Drawing.Size(136, 16);
+            this.extInterfaceFiles_Label.TabIndex = 4;
+            this.extInterfaceFiles_Label.Text = "External Interface files";
+            // 
+            // count_Label
+            // 
+            this.count_Label.AutoSize = true;
+            this.count_Label.Location = new System.Drawing.Point(368, 26);
+            this.count_Label.Name = "count_Label";
+            this.count_Label.Size = new System.Drawing.Size(41, 16);
+            this.count_Label.TabIndex = 5;
+            this.count_Label.Text = "Count";
+            // 
+            // extInput_Numeric
+            // 
+            this.extInput_Numeric.Location = new System.Drawing.Point(329, 70);
+            this.extInput_Numeric.Name = "extInput_Numeric";
+            this.extInput_Numeric.Size = new System.Drawing.Size(120, 22);
+            this.extInput_Numeric.TabIndex = 6;
+            // 
+            // extOutput_Numeric
+            // 
+            this.extOutput_Numeric.Location = new System.Drawing.Point(329, 116);
+            this.extOutput_Numeric.Name = "extOutput_Numeric";
+            this.extOutput_Numeric.Size = new System.Drawing.Size(120, 22);
+            this.extOutput_Numeric.TabIndex = 7;
+            // 
+            // extInquiry_Numeric
+            // 
+            this.extInquiry_Numeric.Location = new System.Drawing.Point(329, 166);
+            this.extInquiry_Numeric.Name = "extInquiry_Numeric";
+            this.extInquiry_Numeric.Size = new System.Drawing.Size(120, 22);
+            this.extInquiry_Numeric.TabIndex = 8;
+            // 
+            // intLogicFiles_Numeric
+            // 
+            this.intLogicFiles_Numeric.Location = new System.Drawing.Point(329, 206);
+            this.intLogicFiles_Numeric.Name = "intLogicFiles_Numeric";
+            this.intLogicFiles_Numeric.Size = new System.Drawing.Size(120, 22);
+            this.intLogicFiles_Numeric.TabIndex = 9;
+            // 
+            // extInterfaceFiles_Numeric
+            // 
+            this.extInterfaceFiles_Numeric.Location = new System.Drawing.Point(329, 249);
+            this.extInterfaceFiles_Numeric.Name = "extInterfaceFiles_Numeric";
+            this.extInterfaceFiles_Numeric.Size = new System.Drawing.Size(120, 22);
+            this.extInterfaceFiles_Numeric.TabIndex = 10;
+            // 
+            // extInput_ComboBox
+            // 
+            this.extInput_ComboBox.FormattingEnabled = true;
+            this.extInput_ComboBox.Items.AddRange(new object[] {
+            "Simple",
+            "Average",
+            "Complex"});
+            this.extInput_ComboBox.Location = new System.Drawing.Point(542, 70);
+            this.extInput_ComboBox.Name = "extInput_ComboBox";
+            this.extInput_ComboBox.Size = new System.Drawing.Size(121, 24);
+            this.extInput_ComboBox.TabIndex = 11;
+            // 
+            // type_Label
+            // 
+            this.type_Label.AutoSize = true;
+            this.type_Label.Location = new System.Drawing.Point(585, 26);
+            this.type_Label.Name = "type_Label";
+            this.type_Label.Size = new System.Drawing.Size(39, 16);
+            this.type_Label.TabIndex = 16;
+            this.type_Label.Text = "Type";
+            // 
+            // extOutput_ComboBox
+            // 
+            this.extOutput_ComboBox.FormattingEnabled = true;
+            this.extOutput_ComboBox.Items.AddRange(new object[] {
+            "Simple",
+            "Average",
+            "Complex"});
+            this.extOutput_ComboBox.Location = new System.Drawing.Point(542, 116);
+            this.extOutput_ComboBox.Name = "extOutput_ComboBox";
+            this.extOutput_ComboBox.Size = new System.Drawing.Size(121, 24);
+            this.extOutput_ComboBox.TabIndex = 17;
+            // 
+            // extInquiry_ComboBox
+            // 
+            this.extInquiry_ComboBox.FormattingEnabled = true;
+            this.extInquiry_ComboBox.Items.AddRange(new object[] {
+            "Simple",
+            "Average",
+            "Complex"});
+            this.extInquiry_ComboBox.Location = new System.Drawing.Point(542, 166);
+            this.extInquiry_ComboBox.Name = "extInquiry_ComboBox";
+            this.extInquiry_ComboBox.Size = new System.Drawing.Size(121, 24);
+            this.extInquiry_ComboBox.TabIndex = 18;
+            // 
+            // intLogicFiles_ComboBox
+            // 
+            this.intLogicFiles_ComboBox.FormattingEnabled = true;
+            this.intLogicFiles_ComboBox.Items.AddRange(new object[] {
+            "Simple",
+            "Average",
+            "Complex"});
+            this.intLogicFiles_ComboBox.Location = new System.Drawing.Point(542, 206);
+            this.intLogicFiles_ComboBox.Name = "intLogicFiles_ComboBox";
+            this.intLogicFiles_ComboBox.Size = new System.Drawing.Size(121, 24);
+            this.intLogicFiles_ComboBox.TabIndex = 19;
+            // 
+            // extInterfaceFiles_ComboBox
+            // 
+            this.extInterfaceFiles_ComboBox.FormattingEnabled = true;
+            this.extInterfaceFiles_ComboBox.Items.AddRange(new object[] {
+            "Simple",
+            "Average",
+            "Complex"});
+            this.extInterfaceFiles_ComboBox.Location = new System.Drawing.Point(542, 249);
+            this.extInterfaceFiles_ComboBox.Name = "extInterfaceFiles_ComboBox";
+            this.extInterfaceFiles_ComboBox.Size = new System.Drawing.Size(121, 24);
+            this.extInterfaceFiles_ComboBox.TabIndex = 20;
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(83, 434);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(609, 180);
+            this.resultTextBox.TabIndex = 21;
+            this.resultTextBox.Text = "";
+            // 
+            // calculateUFP_Button
+            // 
+            this.calculateUFP_Button.Location = new System.Drawing.Point(71, 327);
+            this.calculateUFP_Button.Name = "calculateUFP_Button";
+            this.calculateUFP_Button.Size = new System.Drawing.Size(125, 44);
+            this.calculateUFP_Button.TabIndex = 22;
+            this.calculateUFP_Button.Text = "Calculate UFP";
+            this.calculateUFP_Button.UseVisualStyleBackColor = true;
+            this.calculateUFP_Button.Click += new System.EventHandler(this.calculateUFP_Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(812, 687);
+            this.Controls.Add(this.calculateUFP_Button);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.extInterfaceFiles_ComboBox);
+            this.Controls.Add(this.intLogicFiles_ComboBox);
+            this.Controls.Add(this.extInquiry_ComboBox);
+            this.Controls.Add(this.extOutput_ComboBox);
+            this.Controls.Add(this.type_Label);
+            this.Controls.Add(this.extInput_ComboBox);
+            this.Controls.Add(this.extInterfaceFiles_Numeric);
+            this.Controls.Add(this.intLogicFiles_Numeric);
+            this.Controls.Add(this.extInquiry_Numeric);
+            this.Controls.Add(this.extOutput_Numeric);
+            this.Controls.Add(this.extInput_Numeric);
+            this.Controls.Add(this.count_Label);
+            this.Controls.Add(this.extInterfaceFiles_Label);
+            this.Controls.Add(this.intLogicFiles_Label);
+            this.Controls.Add(this.extInquiry_Label);
+            this.Controls.Add(this.extOutput_Label);
+            this.Controls.Add(this.extInput_Label);
+            this.Name = "Form1";
+            this.Text = "FP Calculator Assistant";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.extInput_Numeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.extOutput_Numeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.extInquiry_Numeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.intLogicFiles_Numeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.extInterfaceFiles_Numeric)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label extInput_Label;
+        private System.Windows.Forms.Label extOutput_Label;
+        private System.Windows.Forms.Label extInquiry_Label;
+        private System.Windows.Forms.Label intLogicFiles_Label;
+        private System.Windows.Forms.Label extInterfaceFiles_Label;
+        private System.Windows.Forms.Label count_Label;
+        private System.Windows.Forms.NumericUpDown extInput_Numeric;
+        private System.Windows.Forms.NumericUpDown extOutput_Numeric;
+        private System.Windows.Forms.NumericUpDown extInquiry_Numeric;
+        private System.Windows.Forms.NumericUpDown intLogicFiles_Numeric;
+        private System.Windows.Forms.NumericUpDown extInterfaceFiles_Numeric;
+        private System.Windows.Forms.ComboBox extInput_ComboBox;
+        private System.Windows.Forms.Label type_Label;
+        private System.Windows.Forms.ComboBox extOutput_ComboBox;
+        private System.Windows.Forms.ComboBox extInquiry_ComboBox;
+        private System.Windows.Forms.ComboBox intLogicFiles_ComboBox;
+        private System.Windows.Forms.ComboBox extInterfaceFiles_ComboBox;
+        private System.Windows.Forms.RichTextBox resultTextBox;
+        private System.Windows.Forms.Button calculateUFP_Button;
     }
 }
 
--- metricsFP/metricsFP/Form1.cs
+++ metricsFP/metricsFP/Form1.cs
@@ -8,13 +8,96 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+public enum Parameter
+{
+    //values to index into the complexity table array
+    ExternalInput = 0,
+    ExternalOutput = 1,
+    ExternalInquiry = 2,
+    InternalLogicalFiles = 3,
+    ExternalInterfaceFiles = 4,
+}
+
+public enum Complexity
+{
+    //values to index into the complexity table array
+    Simple = 0,
+    Average = 1,
+    Complex = 2,
+}
+
+
 namespace metricsFP
 {
+
+
     public partial class Form1 : Form
     {
+        //index this using complexity paramter type and level enums to get desired value
+        private int[,] complexityTable = new int[5, 3]
+        {
+            { 3, 4, 6   },
+            { 4,  5,  7 },
+            { 3,  4,  6 },
+            { 7, 10, 15 },
+            { 5,  7, 10 },
+        };
+        
+
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Set all combo boxes to first value
+            extInput_ComboBox.SelectedIndex = 0;
+            extOutput_ComboBox.SelectedIndex = 0;
+            extInquiry_ComboBox.SelectedIndex = 0;
+            intLogicFiles_ComboBox.SelectedIndex = 0;
+            extInterfaceFiles_ComboBox.SelectedIndex = 0;
+
+        }
+
+        private void calculateUFP_Button_Click(object sender, EventArgs e)
+        {
+            //get count of all parameters based on input
+            int extInputCount = (int) extInput_Numeric.Value;
+            int extOutputCount = (int) extOutput_Numeric.Value;
+            int extInquiryCount = (int) extInquiry_Numeric.Value;
+            int intLogicFilesCount = (int) intLogicFiles_Numeric.Value;
+            int extInterfaceFilesCount = (int) extInterfaceFiles_Numeric.Value;
+
+            //get complexity type of all parameters from the combobox
+            int extInputType = extInput_ComboBox.SelectedIndex;
+            int extOutputType = extOutput_ComboBox.SelectedIndex;
+            int extInquiryType = extInquiry_ComboBox.SelectedIndex;
+            int intLogicFilesType = intLogicFiles_ComboBox.SelectedIndex;
+            int extInterfaceFilesType = extInterfaceFiles_ComboBox.SelectedIndex;
+
+            //index complexity table
+            int extInputComplexity = complexityTable[(int)Parameter.ExternalInput, extInputType];
+            int extOutputComplexity = complexityTable[(int)Parameter.ExternalOutput, extOutputType];
+            int extInquiryComplexity = complexityTable[(int)Parameter.ExternalInquiry, extInquiryType];
+            int intLogicFilesComplexity = complexityTable[(int)Parameter.InternalLogicalFiles, intLogicFilesType];
+            int extInterfaceFilesComplexity = complexityTable[(int)Parameter.ExternalInterfaceFiles, extInterfaceFilesType];
+
+
+            int totalUFP =
+             extInputCount * extInputComplexity +
+             extOutputCount * extOutputComplexity +
+             extInquiryCount * extInquiryComplexity +
+             intLogicFilesCount * intLogicFilesComplexity +
+             extInterfaceFilesCount * extInterfaceFilesComplexity;
+
+            resultTextBox.Text =
+              "Function Point Analysis Result\n" +
+              "------------------------------\n" +
+              $"Total Unadjusted Function Points (UFP): {totalUFP}";
         }
     }
 }
